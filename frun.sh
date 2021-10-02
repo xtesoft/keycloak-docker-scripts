b5378856723072282449f0f9c8689f2288ebf163
Tadeo Rivera <xtecuan@gmail.com>
2021-10-01 22:36:57 -0600

    ddd

--- frun.sh
+++ frun.sh
@@ -3,7 +3,7 @@ export KUSER="admin"
 export KPASS="Welcome123$"
 
 export CONTAINER_NAME=keycloak-mined
-export CONTAINER_IMAGE="quay.io/keycloak/"
+export CONTAINER_IMAGE="quay.io/keycloak/keycloak"
 export POD_NET=network-keycloak
 
 docker run --restart unless-stopped  -d -p 8080:8080 -p 8443:8443  --net=$POD_NET --name=$CONTAINER_NAME $CONTAINER_IMAGE
